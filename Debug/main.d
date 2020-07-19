7ee06fe4ec3a5611e650c2e293fd72ba8cf5fe9c
Allison Power <66786731+allison-7@users.noreply.github.com>
2020-07-19 12:10:18 -0230

    Add files via upload

---
+++ Debug/main.d
@@ -0,0 +1,147 @@
+main.o: ../main.cpp ../Kraken/kraken_api.h ../Kraken/logging.h \
+ ../Kraken/Public/Time.h ../Kraken/Public/../../rapidjson/document.h \
+ ../Kraken/Public/../../rapidjson/reader.h \
+ ../Kraken/Public/../../rapidjson/allocators.h \
+ ../Kraken/Public/../../rapidjson/rapidjson.h \
+ ../Kraken/Public/../../rapidjson/stream.h \
+ ../Kraken/Public/../../rapidjson/encodings.h \
+ ../Kraken/Public/../../rapidjson/encodedstream.h \
+ ../Kraken/Public/../../rapidjson/memorystream.h \
+ ../Kraken/Public/../../rapidjson/internal/meta.h \
+ ../Kraken/Public/../../rapidjson/internal/../rapidjson.h \
+ ../Kraken/Public/../../rapidjson/internal/stack.h \
+ ../Kraken/Public/../../rapidjson/internal/../allocators.h \
+ ../Kraken/Public/../../rapidjson/internal/swap.h \
+ ../Kraken/Public/../../rapidjson/internal/strtod.h \
+ ../Kraken/Public/../../rapidjson/internal/ieee754.h \
+ ../Kraken/Public/../../rapidjson/internal/biginteger.h \
+ ../Kraken/Public/../../rapidjson/internal/diyfp.h \
+ ../Kraken/Public/../../rapidjson/internal/pow10.h \
+ ../Kraken/Public/../../rapidjson/error/error.h \
+ ../Kraken/Public/../../rapidjson/error/../rapidjson.h \
+ ../Kraken/Public/../../rapidjson/internal/strfunc.h \
+ ../Kraken/Public/../../rapidjson/internal/../stream.h \
+ ../Kraken/Public/../../rapidjson/internal/../rapidjson.h \
+ ../Kraken/Public/../kraken_api.h ../Kraken/Public/RecentTrades.h \
+ ../Kraken/Public/OHLC.h ../Kraken/Public/SpreadData.h \
+ ../Kraken/AssetInfo.h ../Kraken/../rapidjson/document.h \
+ ../Kraken/kraken_api.h ../Kraken/Public/AssetPairs.h \
+ ../Kraken/Public/../../rapidjson/pointer.h \
+ ../Kraken/Public/../../rapidjson/document.h \
+ ../Kraken/Public/../../rapidjson/internal/itoa.h \
+ ../Kraken/TickerInformation.h ../Kraken/OrderBook.h \
+ ../Kraken/Private/TradeBalance.h \
+ ../Kraken/Private/../../rapidjson/pointer.h \
+ ../Kraken/Private/../../rapidjson/document.h \
+ ../Kraken/Private/../kraken_api.h ../Kraken/Private/AccountBalance.h \
+ ../Kraken/Private/OpenOrders.h ../Kraken/Private/ClosedOrders.h \
+ ../Kraken/Private/OrdersInfo.h ../Kraken/Private/TradeHistory.h \
+ ../Kraken/Private/TradesInfo.h ../Kraken/Private/LedgersInfo.h \
+ ../Kraken/Private/QueryLedgers.h ../Kraken/Private/TradeVolume.h \
+ ../Kraken/Private/OpenPositions.h
+
+../Kraken/kraken_api.h:
+
+../Kraken/logging.h:
+
+../Kraken/Public/Time.h:
+
+../Kraken/Public/../../rapidjson/document.h:
+
+../Kraken/Public/../../rapidjson/reader.h:
+
+../Kraken/Public/../../rapidjson/allocators.h:
+
+../Kraken/Public/../../rapidjson/rapidjson.h:
+
+../Kraken/Public/../../rapidjson/stream.h:
+
+../Kraken/Public/../../rapidjson/encodings.h:
+
+../Kraken/Public/../../rapidjson/encodedstream.h:
+
+../Kraken/Public/../../rapidjson/memorystream.h:
+
+../Kraken/Public/../../rapidjson/internal/meta.h:
+
+../Kraken/Public/../../rapidjson/internal/../rapidjson.h:
+
+../Kraken/Public/../../rapidjson/internal/stack.h:
+
+../Kraken/Public/../../rapidjson/internal/../allocators.h:
+
+../Kraken/Public/../../rapidjson/internal/swap.h:
+
+../Kraken/Public/../../rapidjson/internal/strtod.h:
+
+../Kraken/Public/../../rapidjson/internal/ieee754.h:
+
+../Kraken/Public/../../rapidjson/internal/biginteger.h:
+
+../Kraken/Public/../../rapidjson/internal/diyfp.h:
+
+../Kraken/Public/../../rapidjson/internal/pow10.h:
+
+../Kraken/Public/../../rapidjson/error/error.h:
+
+../Kraken/Public/../../rapidjson/error/../rapidjson.h:
+
+../Kraken/Public/../../rapidjson/internal/strfunc.h:
+
+../Kraken/Public/../../rapidjson/internal/../stream.h:
+
+../Kraken/Public/../../rapidjson/internal/../rapidjson.h:
+
+../Kraken/Public/../kraken_api.h:
+
+../Kraken/Public/RecentTrades.h:
+
+../Kraken/Public/OHLC.h:
+
+../Kraken/Public/SpreadData.h:
+
+../Kraken/AssetInfo.h:
+
+../Kraken/../rapidjson/document.h:
+
+../Kraken/kraken_api.h:
+
+../Kraken/Public/AssetPairs.h:
+
+../Kraken/Public/../../rapidjson/pointer.h:
+
+../Kraken/Public/../../rapidjson/document.h:
+
+../Kraken/Public/../../rapidjson/internal/itoa.h:
+
+../Kraken/TickerInformation.h:
+
+../Kraken/OrderBook.h:
+
+../Kraken/Private/TradeBalance.h:
+
+../Kraken/Private/../../rapidjson/pointer.h:
+
+../Kraken/Private/../../rapidjson/document.h:
+
+../Kraken/Private/../kraken_api.h:
+
+../Kraken/Private/AccountBalance.h:
+
+../Kraken/Private/OpenOrders.h:
+
+../Kraken/Private/ClosedOrders.h:
+
+../Kraken/Private/OrdersInfo.h:
+
+../Kraken/Private/TradeHistory.h:
+
+../Kraken/Private/TradesInfo.h:
+
+../Kraken/Private/LedgersInfo.h:
+
+../Kraken/Private/QueryLedgers.h:
+
+../Kraken/Private/TradeVolume.h:
+
+../Kraken/Private/OpenPositions.h:
---
+++ Debug/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../main.cpp 
+
+OBJS += \
+./main.o 
+
+CPP_DEPS += \
+./main.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross G++ Compiler'
+	g++ -D_cplusplus=201130L -I/usr/local/ssl/include -O0 -g3 -Wall -c -fmessage-length=0 -std=c++11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
